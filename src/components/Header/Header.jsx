c700f20252285025b5ed1f94415ba9cae69f458b
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-18 18:28:26 +0100

    Creat header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+export default function() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Project</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </nav> 
+        </header>
+    )
+}
