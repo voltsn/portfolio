9529a468b66751a5834d3ee8418f8094bc2239e2
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-21 16:26:53 +0100

    Turn preview elements into a component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,18 @@
 import longArrow from '../../assets/longArrow.svg';
 
-export default function () {
+function Preview({text, link, linkLabel}) {
+    return (
+        <article>
+            <p>{text}</p>
+            <div>
+                <img src={longArrow} alt="Long right pointing arrow"/>
+                <a href={link}>{linkLabel}</a>
+            </div>
+        </article>
+    );
+}
+
+export default function Hero () {
     return (
         <section>
             <h1>
@@ -9,38 +21,22 @@ export default function () {
             </h1>
             
             <div>
-                <article>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magnaa
-                    liqualiqualiquaialialiqua.
-                    </p>
-                    <p> 
-                        <img 
-                            aria-hidden="true" 
-                            src={longArrow} 
-                            alt="long right pointing arrow"
-                        /> 
-                        <a>Learn more about me</a>
-                    </p>
-                </article>
+              <Preview
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing 
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magnaa liqua."
+                    link="#aboutme"
+                    linkLabel="Learn more about me"
+              />
 
-                <article>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magnaa
-                    liqualiqualiquaialialiqua.
-                    </p>
-                    <p> 
-                        <img 
-                            aria-hidden="true" 
-                            src={longArrow} 
-                            alt="long right pointing arrow"
-                        /> 
-                        <a>View my work</a>
-                    </p>
-                </article>
+              <Preview 
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing 
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magnaa liqua."
+                    link="#projects"
+                    linkLabel="View my work"
+              />
             </div>
         </section>
-    )
+    );
 }
