baacc238829274ce3759612f97073283974f9f71
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-27 17:08:36 +0200

    Add css classes

--- src/components/Tools/Tools.jsx
+++ src/components/Tools/Tools.jsx
@@ -1,3 +1,4 @@
+import './Tools.css';
 import laravelIcon  from '../../assets/laravel.svg';
 import reactIcon from '../../assets/react.svg';
 import cssIcon from '../../assets/css3.svg';
@@ -15,10 +16,10 @@ function ToolsItem({icon, label, iconDescription}) {
 
 export default function Tools() {
     return (
-        <section>
-            <h2>Tools of the trade</h2>
-            <div>
-                <ul>
+        <section id='tools' className='tools container'>
+            <h2 className='tools__heading'>Tools of the trade</h2>
+            <div className='tools__content'>
+                <ul className='toolsList'>
                     <ToolsItem 
                         icon={laravelIcon} 
                         iconDescription={"laravel Logo"} 
