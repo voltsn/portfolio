5e950eb8756796ea9ff7f4008a9802d0f8d35b24
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-27 17:03:38 +0200

    Add classes in the hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,12 @@
+import './Hero.css';
 import longArrow from '../../assets/longArrow.svg';
+import gradientCircle from '../../assets/gradient-circle.svg';
 
-function Preview({text, link, linkLabel}) {
+function Preview({text, link, linkLabel, className}) {
     return (
-        <article>
-            <p>{text}</p>
-            <div>
+        <article className={className.article}>
+            <p className={className.p}>{text}</p>
+            <div className={className.div}>
                 <img src={longArrow} alt="Long right pointing arrow"/>
                 <a href={link}>{linkLabel}</a>
             </div>
@@ -14,20 +16,21 @@ function Preview({text, link, linkLabel}) {
 
 export default function Hero () {
     return (
-        <section id='hero'>
-            <h1>
+        <section id='hero' className='hero container'>
+            <h1 className='hero__heading'>
                 Arno N. Volts
-                <span>Junior Web Developer</span>
+                <span className='hero__subHeading'>Junior Web Developer</span>
             </h1>
             
-            <div>
+            <div className='previews'>
               <Preview
                     text="Lorem ipsum dolor sit amet, consectetur adipiscing 
                           elit, sed do eiusmod tempor incididunt ut labore et
                           dolore magnaa liqua."
                     link="#aboutme"
                     linkLabel="Learn more about me"
-              />
+                    className={{article: 'preview', p: 'preview__description', div: 'preview__link'}}
+                />
 
               <Preview 
                     text="Lorem ipsum dolor sit amet, consectetur adipiscing 
@@ -35,8 +38,17 @@ export default function Hero () {
                           dolore magnaa liqua."
                     link="#projects"
                     linkLabel="View my work"
-              />
+                    className={{article: 'preview', p: 'preview__description', div: 'preview__link'}}
+                />
+
             </div>
+            <img className='gradientCircle top' 
+                src={gradientCircle} 
+                alt="gradient circle" 
+            />
+            <img className='gradientCircle bottom'
+                src={gradientCircle} 
+                alt="gradient circle" />
         </section>
     );
 }
